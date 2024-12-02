6aa3c1b395ac6956d830d8d3dc787f5dd2f1b8d3
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-02 11:50:40 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -20,7 +20,7 @@ export NEZHA_KEY=${NEZHA_KEY:-''}
 
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
-ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
+#ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
 
 
 read_uuid() {
