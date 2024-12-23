7c462d88d8f94c30005b2c1524cd002e87a53eb8
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-23 14:41:21 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -18,16 +18,6 @@ HOSTNAME=$(hostname)
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
 ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
 
-if [ -z "${ARGO_DOMAIN}" ]; then
-sed -i '' -e '32s|111||' 1.sh
-sed -i '' -e '33s|999||' 1.sh
-echo "${ARGO_DOMAIN}"
-else
-sed -i '' -e '32s|111|'"$ARGO_DOMAIN"'|' 1.sh
-sed -i '' -e '33s|999|'"$ARGO_AUTH"'|' 1.sh
-echo "${ARGO_DOMAIN}"
-fi
-
 export UUID=${UUID:-'743f8207-40d0-4440-9a44-97be0fea69c1'}  
 export ARGO_DOMAIN=${ARGO_DOMAIN:-'111'}   
 export ARGO_AUTH=${ARGO_AUTH:-'999'}     
@@ -36,6 +26,14 @@ export vmess_port=${vmess_port:-'456'}
 export hy2_port=${hy2_port:-'789'}       
 export IP=${IP:-'888'}                  
 export reym=${reym:-'www.speedtest.net'}
+
+if [ -z "${ARGO_DOMAIN8}" ]; then
+sed -i '' -e '22s|111||' 1.sh
+sed -i '' -e '23s|999||' 1.sh
+else
+sed -i '' -e '22s|111|'"$ARGO_DOMAIN8"'|' 1.sh
+sed -i '' -e '23s|999|'"$ARGO_AUTH8"'|' 1.sh
+fi
 
 echo "ARGO_DOMAIN=$ARGO_DOMAIN"
 echo "ARGO_AUTH=$ARGO_AUTH"
