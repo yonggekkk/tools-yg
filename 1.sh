19697bf76309670bc60be1e35e2e7281ca32b7a7
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-26 08:19:15 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -40,6 +40,7 @@ read_ip(){
 nb=$(echo "$HOSTNAME" | cut -d '.' -f 1 | tr -d 's')
 ym=("$HOSTNAME" "cache$nb.serv00.com" "web$nb.serv00.com")
 rm -rf $WORKDIR/ip.txt
+mkdir $WORKDIR/ip.txt
 for ym in "${ym[@]}"; do
 # 引用frankiejun API
 response=$(curl -sL --connect-timeout 5 --max-time 7 "https://ss.botai.us.kg/api/getip?host=$ym")
