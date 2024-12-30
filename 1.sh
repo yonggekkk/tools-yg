a60772ec97b72fa51f32ad01c471b0258140dc2b
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-30 12:54:56 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -238,21 +238,21 @@ fi
     }
  ],
     "outbounds": [
-         {
+     {
         "type": "wireguard",
         "tag": "wg",
-        "server": "162.159.192.100",
-        "server_port": 2408,
+        "server": "162.159.195.100",
+        "server_port": 4500,
         "local_address": [
-        "172.16.0.2/32",
-        "2606:4700:110:8468:c6c3:c1a2:2db1:a7a/128"
+                "172.16.0.2/32",
+                "2606:4700:110:83c7:b31f:5858:b3a8:c6b1/128"
         ],
-        "private_key": "hveWdmx6gLzabPneunzSvj0zDfYVYXq++b0kRuKdGq8=",
+        "private_key": "mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=",
         "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
         "reserved": [
-            165,
-            196,
-            69
+                26,
+                21,
+                228
         ]
     },
     {
