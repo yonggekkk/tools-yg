ce507cb4ec003de486e8eb675543b21cebcc39a9
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-25 15:55:01 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1092,19 +1092,16 @@ fi
 }
 
 keepweb(){
-    keep_path="$HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs"
-    [ -d "$keep_path" ] || mkdir -p "$keep_path"
-    
-    devil www add keep.${USERNAME}.serv00.net nodejs /usr/local/bin/node18 > /dev/null 2>&1
-    ln -fs /usr/local/bin/node18 ~/bin/node > /dev/null 2>&1
-    ln -fs /usr/local/bin/npm18 ~/bin/npm > /dev/null 2>&1
-    mkdir -p ~/.npm-global
-    npm config set prefix '~/.npm-global'
-    echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
-    cd ${keep_path} && npm install dotenv basic-auth express > /dev/null 2>&1
+mkdir -p ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
+devil www del ${USERNAME}.serv00.net
+rm -rf /home/${USERNAME}/domains/${USERNAME}.serv00.net
+devil www add ${USERNAME}.serv00.net nodejs $(command -v node22) production
 mv /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs/public /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs/static
-curl -sL https://raw.githubusercontent.com/yonggekkk/tools-yg/main/app.js -o $HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs/app.js
-    devil www restart keep.${USERNAME}.serv00.net
+curl -sL https://raw.githubusercontent.com/yonggekkk/tools-yg/main/app.js -o $HOME/domains/${USERNAME}.serv00.net/public_nodejs/app.js
+cd /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs
+npm install dotenv basic-auth express
 }
 
 showsbclash(){
