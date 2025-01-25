36f35f8f30eb4dacee08c127e639993398a19e40
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-25 11:07:35 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1095,12 +1095,6 @@ fi
 
 keepweb(){
 devil binexec on
-
-# 存储用户名并转换为小写
-USERNAME=${USER}
-LC_USERNAME=$(echo ${USER} | tr '[:upper:]' '[:lower:]')
-
-# 安装 PM2
 if [ -z "$(command -v pm2)" ]; then
     mkdir -p /home/${USERNAME}/.npm-global
     npm config set prefix '/home/${USERNAME}/.npm-global'
@@ -1112,19 +1106,13 @@ if [ -z "$(command -v pm2)" ]; then
     npm install -g pm2
 fi
 . /home/${USERNAME}/.profile
-
-# 删除默认网站
-devil www del ${LC_USERNAME}.serv00.net
-rm -rf /home/${USERNAME}/domains/${LC_USERNAME}.serv00.net
-
-# 创建网站
-devil www add ${LC_USERNAME}.serv00.net nodejs $(command -v node22) production
-
-# 安装网页
-mv /home/${USERNAME}/domains/${LC_USERNAME}.serv00.net/public_nodejs/public /home/${USERNAME}/domains/${LC_USERNAME}.serv00.net/public_nodejs/static
-curl -sL https://raw.githubusercontent.com/yonggekkk/tools-yg/main/web.js -o $HOME/domains/${USERNAME}.serv00.net/public_nodejs/app.js
-sed -i '' "s/USERNAME_TO_REPLACE/${USERNAME}/g" "/home/${USERNAME}/domains/${LC_USERNAME}.serv00.net/public_nodejs/app.js"
-cd /home/${USERNAME}/domains/${LC_USERNAME}.serv00.net/public_nodejs
+devil www del ${USERNAME}.serv00.net
+rm -rf /home/${USERNAME}/domains/${USERNAME}.serv00.net
+devil www add ${USERNAME}.serv00.net nodejs $(command -v node22) production
+mv /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs/public /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs/static
+curl -sL https://raw.githubusercontent.com/yonggekkk/tools-yg/main/app.js -o $HOME/domains/${USERNAME}.serv00.net/public_nodejs/app.js
+sed -i '' "s/USERNAME_TO_REPLACE/${USERNAME}/g" "/home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs/app.js"
+cd /home/${USERNAME}/domains/${USERNAME}.serv00.net/public_nodejs
 npm22 install express
 }
 
