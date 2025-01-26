acccb6cc2d1367c54f335321facc4a56bcdbb1d5
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-26 21:36:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.get("/up", function (req, res) {
         console.log("命令执行成功:\n" + stdout);
     });
 
-    res.type("html").send("<pre>Serv00！该起床啦！！！</pre>");
+    res.type("html").send("<pre>Serv00网页保活启动：Serv00！一柱擎天！UP！UP！UP！</pre>");
 });
 app.use((req, res, next) => {
     if (req.path === '/up') {
         return next();
     }
-    res.status(404).send('页面未找到');
+    res.status(404).send('把浏览器地址改为：https://你的serv00用户名.你的serv00用户名.serv00.net/up 这样才能启动Serv00网页保活');
 });
 
 app.listen(3000, () => {
