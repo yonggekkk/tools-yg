bf59da6833ef4c2a8320efe498b3498e9154008f
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-01-27 08:50:27 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1131,7 +1131,7 @@ echo
 green "开始安装网页进程保活"
 keep_path="$HOME/domains/${USERNAME}.${USERNAME}.serv00.net/public_nodejs"
 [ -d "$keep_path" ] || mkdir -p "$keep_path"
-curl -sL https://raw.githubusercontent.com/yonggekkk/tools-yg/main/app.js -o "$keep_path"/app.js
+curl -sL https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/app.js -o "$keep_path"/app.js
 sed -i "26s/name/$USERNAME/" "$keep_path"
 devil www del ${USERNAME}.${USERNAME}.serv00.net > /dev/null 2>&1
 devil www add ${USERNAME}.serv00.net php > /dev/null 2>&1
