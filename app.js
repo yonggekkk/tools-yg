01a2c09038eaba70f945f751d8be49a4e9d94e52
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-02-14 11:00:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,39 +3,27 @@ const express = require("express");
 const { exec } = require('child_process');
 const app = express();
 app.use(express.json());
-
 const commandToRun = "cd ~ && bash serv00keep.sh";
-
 function runCustomCommand() {
     exec(commandToRun, (err, stdout, stderr) => {
         if (err) console.error("执行错误:", err);
         else console.log("执行成功:", stdout);
     });
 }
-
 app.get("/up", (req, res) => {
     runCustomCommand();
-    res.send("<pre>Serv00服务器保活已启动</pre>");
+    res.type("html").send("<pre>Serv00-name服务器网页保活启动：Serv00-name！UP！UP！UP！</pre>");
 });
-
 app.get("/re", (req, res) => {
     const additionalCommands = `
         USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
         FULL_PATH="/home/\${USERNAME}/domains/\${USERNAME}.serv00.net/logs"
-        if [ ! -d "\$FULL_PATH" ]; then
-            echo "错误：路径\$FULL_PATH不存在"
-            exit 1
-        fi
-        cd "\$FULL_PATH" || exit 1
-        pkill -f 'run -c con' || echo "无进程可终止"
+        cd "\$FULL_PATH"
+        pkill -f 'run -c con' || echo "无进程可终止，准备执行重启……"
         sbb="\$(cat sb.txt 2>/dev/null)"
-        if [ -z "\$sbb" ]; then
-            echo "错误：sb.txt内容为空或文件不存在"
-            exit 1
-        fi
         nohup ./"\$sbb" run -c config.json >/dev/null 2>&1 &
         sleep 3
-        echo '重启成功'
+        echo 'Serv00主程序重启成功，请检测三个主节点是否可用，如不可用，可再次刷新重启网页或者卸载重装脚本'
     `;
     exec(additionalCommands, (err, stdout, stderr) => {
         console.log('stdout:', stdout);
@@ -46,7 +34,6 @@ app.get("/re", (req, res) => {
         res.type('text').send(stdout);
     });
 }); 
-
 app.get("/list", (req, res) => {
     const listCommands = `
         USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
@@ -61,13 +48,10 @@ app.get("/list", (req, res) => {
         res.type('text').send(stdout);
     });
 });
-
 app.use((req, res) => {
-    res.status(404).send('路径未找到');
+    res.status(404).send('网页路径写错了吧？/up是保活，/re是重启，/list是节点信息');
 });
-
 setInterval(runCustomCommand, 3 * 60 * 1000);
-
 app.listen(3000, () => {
     console.log("服务器运行在端口 3000");
     runCustomCommand();
